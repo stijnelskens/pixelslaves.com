42c583a7ceb290c2e47c674b9954ae7cb634cfe1
DESKTOP-NE2OV42\stijn <stijn.elskens@gmail.com>
2022-03-18 00:02:52 +0100

    use env file

--- pages/api/contentful.ts
+++ pages/api/contentful.ts
@@ -1,6 +1,6 @@
 const client = require('contentful').createClient({
-  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-  space: process.env.CONTENTFUL_SPACE_ID,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
- 
+
 export default client;
