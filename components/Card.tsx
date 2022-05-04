60b7ca0380428a1494b5762567cc308e7b184487
DESKTOP-NE2OV42\stijn <stijn.elskens@gmail.com>
2022-05-04 22:57:48 +0200

    a11y and seo tweaks

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,17 +5,17 @@ const Card = ({player}: Player) => {
   const { name, twitter, twitch, instagram, tiktok } = player.fields;
   return (
     <div className={`flex flex-col justify-center p-6 border-2 rounded-lg ${name == 'PixelSlaves_' ? 'border-red-400' : 'border-gray-200'}`} key={player.sys.id}>
-      <h3 className="mb-2 text-2xl font-bold">{name}</h3>
+      <h2 className="mb-2 text-2xl font-bold">{name}</h2>
       {player.metadata.tags[0] && (
         <div className="mb-4">
           <span className="text-gray-500 capitalize">{player.metadata.tags[0].sys.id}</span>
         </div>
       )}
       {/* {twitter && twitch && instagram && tiktok && ( */}
-        <ul className="flex space-x-2">
+        <ul className="flex space-x-3">
           {twitter && (
             <li>
-              <a href={twitter} target="_blank" rel="noopener" className="text-xl hover:text-blue-400">
+              <a href={twitter} target="_blank" rel="noopener" className="text-2xl sm:text-xl hover:text-blue-400">
                 <span className="sr-only">Twitter</span>
                 <IoLogoTwitter />
               </a>
@@ -23,7 +23,7 @@ const Card = ({player}: Player) => {
           )}
           {twitch && (
           <li>
-            <a href={twitch} target="_blank" rel="noopener" className="text-xl hover:text-violet-700">
+            <a href={twitch} target="_blank" rel="noopener" className="text-2xl sm:text-xl hover:text-violet-700">
               <span className="sr-only">Twitch</span>
               <IoLogoTwitch />
             </a>
@@ -31,7 +31,7 @@ const Card = ({player}: Player) => {
           )}
           {instagram && (
             <li>
-              <a href={instagram} target="_blank" rel="noopener" className="text-xl hover:text-orange-400">
+              <a href={instagram} target="_blank" rel="noopener" className="text-2xl sm:text-xl hover:text-orange-400">
                 <span className="sr-only">Instagram</span>
                 <IoLogoInstagram />
               </a>
@@ -39,7 +39,7 @@ const Card = ({player}: Player) => {
           )}
           {tiktok && (
           <li>
-            <a href={tiktok} target="_blank" rel="noopener" className="text-xl hover:text-red-400">
+            <a href={tiktok} target="_blank" rel="noopener" className="text-2xl sm:text-xl hover:text-red-400">
               <span className="sr-only">TikTok</span>
               <IoLogoTiktok />
             </a>
