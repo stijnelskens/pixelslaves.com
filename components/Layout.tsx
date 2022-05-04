014b3dcabe012d699d243fd26d2f459b3b2add20
DESKTOP-NE2OV42\stijn <stijn.elskens@gmail.com>
2022-05-04 22:32:24 +0200

    add og description

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,6 +22,7 @@ const Layout = ({ children, header, title = 'This is the default title' }: Props
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta property="og:title" content="PixelSlaves.com" key="title" />
         <meta property="og:image" content="https://res.cloudinary.com/dg9w1qoqg/image/upload/v1651695332/PixelSlaves/PixelSlaves_wallpaper2_fvdj7i.jpg" />
+        <meta property="og:description" content="Welkom op de officiële website van de PixelSlaves!" />
         <meta name="description" content="Welkom op de officiële website van de PixelSlaves!" />
         <meta name="keywords" content="Gaming, Call of Duty, Warzone, DayZ, Community, Twitch" />
         <Favicon />
