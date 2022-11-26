0d6d03be88cb5291c12ec45fb60083b39e000869
DESKTOP-NE2OV42\stijn <stijn.elskens@gmail.com>
2022-11-26 14:24:45 +0100

    cleanup

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -1,6 +1,6 @@
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout'
 import client from './api/contentful';
-import { useEffect, useState } from 'react';
 import Card from '../components/Card';
 import Loader from '../components/Loader';	
 
