6dd0068ba5b6b7434358bed50b4f1b1f37294508
Stijn E <stijn.elskens@riskconcile.com>
2024-03-04 00:22:23 +0100

    add proper nuxt

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { type Post } from "~/types/Post";
+
+const query = groq`*[ _type == "player" ] | order(_createdAt desc)`;
+const { data: posts } = await useSanityQuery<Post[]>(query);
+</script>
+
+<template>
+  <section>
+    <Card v-if="posts" v-for="post in posts" :key="post._id" :post="post" />
+    <!-- <Welcome /> -->
+  </section>
+</template>
