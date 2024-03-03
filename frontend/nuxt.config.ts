b7660a9f76f2950a480e270ee92fb3d2bf38a6e1
Stijn E <stijn.elskens@riskconcile.com>
2024-03-03 19:38:05 +0100

    Delete unnecessary files and dependencies

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/sanity"],
   typescript: {
-    shim: false
-  }
-})
+    shim: false,
+  },
+  sanity: {
+    projectId: process.env.NUXT_ENV_SANITY_PROJECT_ID,
+    dataset: process.env.NUXT_ENV_SANITY_DATASET,
+    useCdn: true, // `false` if you want to ensure fresh data
+    apiVersion: "2021-03-25",
+  },
+});
--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { AppProps } from 'next/app';
-import '../css/index.css';
-
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
-
-export default App;
