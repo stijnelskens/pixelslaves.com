b66c5248c1817f04c844f45a1c04b8d379f33d10
Stijn E <stijn.elskens@riskconcile.com>
2024-05-14 23:42:16 +0200

    wip

---
+++ components/CodTable.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+const columns = [
+  {
+    key: "id",
+    label: "#",
+  },
+  {
+    key: "player",
+    label: "Player",
+    sortable: true,
+  },
+  {
+    key: "kd",
+    label: "KD Ratio",
+    sortable: true,
+  },
+  {
+    key: "kills",
+    label: "Kills",
+    sortable: true,
+    direction: "desc" as const,
+  },
+  {
+    key: "killrecord",
+    label: "Kill Record",
+    sortable: true,
+  },
+  {
+    key: "wins",
+    label: "Wins",
+    sortable: true,
+  },
+  {
+    key: "score",
+    label: "Score / Min",
+    sortable: true,
+  },
+];
+
+const people = [
+  {
+    id: 1,
+    player: "Lindsay Walton",
+    kd: "Front-end Developer",
+    kills: "lindsay.walton@example.com",
+    role: "Member",
+  },
+  {
+    id: 2,
+    player: "Courtney Henry",
+    kd: "Designer",
+    kills: "courtney.henry@example.com",
+    role: "Admin",
+  },
+  {
+    id: 3,
+    player: "Tom Cook",
+    kd: "Director of Product",
+    kills: "tom.cook@example.com",
+    role: "Member",
+  },
+  {
+    id: 4,
+    player: "Whitney Francis",
+    kd: "Copywriter",
+    kills: "whitney.francis@example.com",
+    role: "Admin",
+  },
+  {
+    id: 5,
+    player: "Leonard Krasner",
+    kd: "Senior Designer",
+    kills: "leonard.krasner@example.com",
+    role: "Owner",
+  },
+  {
+    id: 6,
+    player: "Floyd Miles",
+    kd: "Principal Designer",
+    kills: "floyd.miles@example.com",
+    role: "Member",
+  },
+];
+</script>
+
+<template>
+  <UTable :columns="columns" :rows="people" />
+</template>
---
+++ pages/cod-stats.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="pb-10">
+    <div class="container px-4 mx-auto">
+      <h1 class="sr-only">Players</h1>
+      <CodTable />
+    </div>
+  </section>
+</template>
