5fcc8e424130200a35ec7c97731f06d966d039ce
Stijn E <stijn.elskens@riskconcile.com>
2024-05-16 08:47:35 +0200

    wip

--- pages/cod-stats.vue
+++ pages/cod-stats.vue
@@ -3,6 +3,7 @@
     <div class="container px-4 mx-auto">
       <h1 class="sr-only">Players</h1>
       <CodTable />
+      <UButton label="Fetch data" class="mt-4" />
     </div>
   </section>
 </template>
